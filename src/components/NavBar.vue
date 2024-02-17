794b99544f6b30e4c94295400fe3226c37470419
AIO <69240351+AIO-Develope@users.noreply.github.com>
2024-02-17 21:54:39 +0100

    Update NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -8,43 +8,7 @@
     </header>
     <router-view></router-view>
   </div>
-  <div class="container d-flex justify-content-between align-items-center">
-      <h1 class="m-0">
-        <a href="#" class="logo">Aromax</a>
-      </h1>
-      <nav class="navbar navbar-expand-lg navbar-light">
-        <div class="container-fluid">
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link" href="#home">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#services">Services</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#features">Features</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#about">About</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#stats">Stats</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#contact">Contact Us</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-      <form action="/discord">
-        <button class="btn btn-secondary">Join</button>
-      </form>
-    </div>
+  
 </template>
 
 <script>
